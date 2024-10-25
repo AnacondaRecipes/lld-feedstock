413b8d83e5873190f387379db0eae645748aca37
Charles Bousseau <cbousseau@anaconda.com>
2024-10-25 15:02:27 -0400

    update

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,10 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
     CMAKE_ARGS="$CMAKE_ARGS -DLLVM_CONFIG_PATH=$BUILD_PREFIX/bin/llvm-config -DLLVM_TABLEGEN_EXE=$BUILD_PREFIX/bin/llvm-tblgen"
 fi
 
+if [[ "${target_platform}" == osx-* ]]; then
+    CMAKE_ARGS="$CMAKE_ARGS -DLLVM_ENABLE_LIBCXX=ON"
+fi
+
 cmake \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
@@ -16,9 +20,8 @@ cmake \
   -DLLVM_INCLUDE_TESTS=OFF \
   -DLLVM_OBJ_ROOT=${PREFIX} \
   -DLLVM_MAIN_INCLUDE_DIR=${PREFIX}/include \
-  -DLLVM_CMAKE_PATH=${PREFIX}/lib/cmake/llvm \
   ${CMAKE_ARGS} \
   ../lld
 
-make -j${CPU_COUNT}
-make install
+cmake --build . -- -j${CPU_COUNT}
+cmake --install .
